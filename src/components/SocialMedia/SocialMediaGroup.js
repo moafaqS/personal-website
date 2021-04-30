d25034b4a073994f6cd9b80c220847dfaf6c44fb
moafaqs <moafaq>
2021-04-30 03:27:09 +0300

    social media links added

--- src/components/SocialMedia/SocialMediaGroup.js
+++ src/components/SocialMedia/SocialMediaGroup.js
@@ -1,27 +1,30 @@
-import React from "react"
-import styled from "styled-components"
-import SocialMediaButton from "../SocialMedia/SocialMediaButton"
+import React from "react";
+import styled from "styled-components";
+import SocialMediaButton from "../SocialMedia/SocialMediaButton";
 
 function SocialMediaGroup() {
   var list = [
-    ["/images/linkedin.svg", "https://www.google.com/"],
-    ["/images/Twitter.svg", "https://www.google.com/"],
-    ["/images/instgram.svg", "https://www.google.com/"],
-  ]
+    ["/images/linkedin.svg", "https://www.linkedin.com/in/mayaser-simbawa/"],
+    ["/images/Twitter.svg", "/"],
+    [
+      "/images/instgram.svg",
+      "https://www.instagram.com/mayaser/?igshid=wdhgltb5owbq",
+    ],
+  ];
 
   return (
     <Wrapper>
-      {list.map(s => (
+      {list.map((s) => (
         <SocialMediaButton key={s} image={s[0]} link={s[1]} />
       ))}
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   justify-items: center;
-`
+`;
 
-export default SocialMediaGroup
+export default SocialMediaGroup;
